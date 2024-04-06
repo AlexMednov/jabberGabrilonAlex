6e31a5d819d090f7a8a92f75b1227ca1893a4834
Alex Mednov <114412787+AlexMednov@users.noreply.github.com>
2024-04-06 18:18:45 +0200

    Created the basis for the command, more needs to be implemented, working on it

--- src/controllers/menuController/command/New.java
+++ src/controllers/menuController/command/New.java
@@ -1,6 +1,20 @@
 package controllers.menuController.command;
 
 public class New implements Command{
+    private String receiver;
+
+    public New(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
     @Override
     public void execute() {
 
--- src/controllers/menuController/command/Save.java
+++ src/controllers/menuController/command/Save.java
@@ -1,6 +1,30 @@
 package controllers.menuController.command;
 
 public class Save implements Command{
+    private Reciever receiver;
+    private String savePath;
+
+    public Save(Reciever receiver, String savePath) {
+        this.receiver = receiver;
+        this.savePath = savePath;
+    }
+
+    public Reciever getReciever() {
+        return receiver;
+    }
+
+    public void setReceiver(Reciever reciever) {
+        this.receiver = reciever;
+    }
+
+    public String getSavePath() {
+        return savePath;
+    }
+
+    public void setSavePath(String savePath) {
+        this.savePath = savePath;
+    }
+
     @Override
     public void execute() {
 
