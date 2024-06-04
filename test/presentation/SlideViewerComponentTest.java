8c324c61d84f97491ef49fb879eb326151545713
Alex Mednov <114412787+AlexMednov@users.noreply.github.com>
2024-06-04 15:07:08 +0200

    Finished testing for slideviewercomponent

--- test/presentation/SlideViewerComponentTest.java
+++ test/presentation/SlideViewerComponentTest.java
@@ -1,9 +1,13 @@
 package presentation;
 
 import org.junit.jupiter.api.Test;
+import slide.Slide;
+import slide.SlideItem;
+import slide.TextItem;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,5 +21,99 @@ class SlideViewerComponentTest extends JFrame {
         SlideViewerComponent slideViewerComponent = new SlideViewerComponent(presentation, this);
 
         assertEquals(labelFont, slideViewerComponent.getLabelFont());
+    }
+
+    @Test
+    void updatingTheSlideViewerComponent_expectNoException(){
+        Presentation presentation = new Presentation();
+
+        JFrame jFrame = new JFrame();
+
+        Slide slide = new Slide();
+
+        SlideViewerComponent slideViewerComponent = new SlideViewerComponent(presentation, jFrame);
+
+        assertDoesNotThrow(()->{
+            presentation.append(slide);
+            slideViewerComponent.update(presentation, slide);
+        });
+    }
+
+    @Test
+    void gettingDimensions_expectNoExceptions_comparingDimensions(){
+        Presentation presentation = new Presentation();
+
+        JFrame jFrame = new JFrame();
+
+        SlideViewerComponent slideViewerComponent = new SlideViewerComponent(presentation, jFrame);
+
+        Dimension dimension = slideViewerComponent.getPreferredSize();
+
+        assertEquals(800, dimension.height);
+    }
+
+    @Test
+    void gettingDimensions_expectNoExceptions_comparingDimensions2(){
+        Presentation presentation = new Presentation();
+
+        JFrame jFrame = new JFrame();
+
+        SlideViewerComponent slideViewerComponent = new SlideViewerComponent(presentation, jFrame);
+
+        Dimension dimension = slideViewerComponent.getPreferredSize();
+
+        assertEquals(1200, dimension.width);
+    }
+
+    @Test
+    void gettingAndSettingSlide_expectNoException_comparingTitles(){
+        Presentation presentation = new Presentation();
+
+        JFrame jFrame = new JFrame();
+
+        SlideViewerComponent slideViewerComponent = new SlideViewerComponent(presentation, jFrame);
+
+        Slide slide = new Slide();
+
+        slideViewerComponent.setSlide(slide);
+
+        assertEquals(slide.getTitle(), slideViewerComponent.getSlide().getTitle());
+    }
+
+    @Test
+    void gettingAndSettingPresentation_expectNoException_comparingTitles(){
+        Presentation presentation = new Presentation();
+
+        JFrame jFrame = new JFrame();
+
+        SlideViewerComponent slideViewerComponent = new SlideViewerComponent(presentation, jFrame);
+
+        TextItem textItem = new TextItem(1, "text");
+
+        Vector<SlideItem> slideItems = new Vector<>();
+        slideItems.add(textItem);
+
+        Slide slide = new Slide("Title", slideItems);
+
+        presentation.append(slide);
+
+        slideViewerComponent.setPresentation(presentation);
+
+        assertEquals("Title", slideViewerComponent.getPresentation().getSlide(0).getTitle());
+    }
+
+    @Test
+    void gettingAndSettingJframe_expectNoException(){
+        Presentation presentation = new Presentation();
+
+        JFrame jFrame = new JFrame();
+
+        SlideViewerComponent slideViewerComponent = new SlideViewerComponent(presentation, jFrame);
+
+        JFrame newJFrame = new JFrame();
+
+        assertDoesNotThrow(()->{
+            slideViewerComponent.setFrame(newJFrame);
+        });
     }
 }
